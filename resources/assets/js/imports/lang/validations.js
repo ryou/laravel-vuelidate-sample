d9e5d834a693486302e416801e59b3b460642bf9
admin <visualjupiter@gmail.com>
2019-02-11 21:12:39 +0900

    fix: パスワード確認時のバリデーションメッセージが正常に表示されていない不具合の修正

--- resources/assets/js/imports/lang/validations.js
+++ resources/assets/js/imports/lang/validations.js
@@ -1,6 +1,6 @@
 export default {
     'required': '必須項目です。',
     'email': '無効なメールアドレスです。',
-    'saveAsPassword': 'パスワードの文字列が異なっています。',
+    'sameAsPassword': 'パスワードの文字列が異なっています。',
     'minLength': '入力値が短すぎます。',
 };
